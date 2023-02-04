1c104b1f3f92fc0fed5f5d313a3c6be215e6231e
Mikael95 <70201151+Mikael95@users.noreply.github.com>
2023-02-04 20:08:39 +0000

    Added Chromedrive.exe

--- API/Scripts/Scraper.py
+++ API/Scripts/Scraper.py
@@ -1,4 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
 
-page = requests.get('https://quotes.toscrape.com')
+
+options = webdriver.ChromeOptions()
+options.binary_location = "C:\Program Files\Google\Chrome\Application"
+chrome_driver_binary = "./chromedriver.exe"
+driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+
+driver.get('https://booking.smyrilline.fo/en/book/multi-journey/journeySearch/')
+time.sleep(5)
+
+search_box = driver.find_element("name", "q")
+search_box.send_keys('ChromeDriver')
+search_box.submit()
